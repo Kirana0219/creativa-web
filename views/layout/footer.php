3481f9511ef73f53e85a8f0573ac7161774361ba
Kiranaaa <putrikirana203@gmail.com>
2026-07-06 21:30:58 +0800

    nambah footer

---
+++ views/layout/footer.php
@@ -0,0 +1,9 @@
+    </main>
+    <footer class="footer text-dark text-center">
+        <div class="container py-3">
+            <p class="mb-0">&copy; <?= date('Y') ?> Creativa. All rights reserved.</p>
+        </div>
+    </footer>
+    <script src="./assets/js/script.js?v=2"></script>
+</body>
+</html>
