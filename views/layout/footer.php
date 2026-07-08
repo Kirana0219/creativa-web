8f837bd3707ed45e2df69ecddb6cbb9ecf07bb66
Kiranaaa <putrikirana203@gmail.com>
2026-07-08 19:36:44 +0800

    menambahkan sw ke delete

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,11 +1,18 @@
     </div>
+
     <footer class="footer text-dark text-center">
         <div class="container py-3">
             <p class="mb-0">&copy; <?= date('Y') ?> Creativa. All rights reserved.</p>
         </div>
     </footer>
+
+    <!-- Library -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- Custom Script -->
     <?php $scriptVersion = file_exists('assets/js/script.js') ? filemtime('assets/js/script.js') : time(); ?>
     <script src="./assets/js/script.js?v=<?= $scriptVersion; ?>"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
